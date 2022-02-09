3400dfebbc0bd4e8e560c69cff4aadb3655d3c35
StnDiego <marcolinodiego.stn@gmail.com>
2022-02-09 20:27:44 -0300

    Instalação de dependecias

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-const http = require('http');
+const express = require('express');
+const consing = require('consign');
+const bodyparser = require('body-parser');
 
-let server = http.createServer((req, res) =>{
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
+app.use(bodyparser.json());
 
-    switch (req.url){
+consing().include('routes').into(app);
 
-        case '/':
-        
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Olá</h1>');
-
-        break;
-
-        case '/users':
-
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-
-                users: [{
-                    name:'Diego M',
-                    email:'marcolinodiego@gmail.com',
-                    id:1
-                }]
-
-            }));
-
-        break;
-
-    }
-
-});
-
-server.listen(3000, '127.0.0.1', ()=>{
+app.listen(3000, '127.0.0.1', ()=>{
 
     console.log('Servidor rodando!');
 
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+module.exports = (app)=>{
+
+    app.get('/users', (req, res)=>{
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+    
+             users: [{
+                name:'Diego M',
+                email:'marcolinodiego@gmail.com',
+                id:1
+    
+            }]
+        });
+    
+    });
+    
+    app.post('/users', (req, res)=>{
+
+        res.json(req.body);
+
+    
+    });
+    
+
+
+};
