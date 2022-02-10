9b1b9c4d7aa5810d41b802bde93d08deae30305e
StnDiego <marcolinodiego.stn@gmail.com>
2022-02-10 14:19:13 -0300

    Versão final get, post, put e Delete funcionando.  Status: ok

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const consing = require('consign');
 const bodyparser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
 app.use(bodyparser.urlencoded({extended: false }));
 app.use(bodyparser.json());
+app.use(expressValidator());
 
-consing().include('routes').into(app);
+consing().include('routes').include('utils').into(app);
 
 app.listen(3000, '127.0.0.1', ()=>{
 
